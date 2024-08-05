8307e541657f632313487e6a10e9b57379ec457f
WrL1n <80534285+WrL1n@users.noreply.github.com>
2024-08-05 11:15:05 +0400

    chore: add loading state for button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,6 +3,7 @@ import { type VariantProps, cva } from "class-variance-authority"
 import * as React from "react"
 
 import { cn } from "@/shared/clsx"
+import { Spinner } from "."
 
 export const buttonVariants = cva(
   "inline-flex items-center justify-center gap-x-2.5 whitespace-nowrap rounded-[5px] text-text-button transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:bg-strokes",
@@ -11,21 +12,13 @@ export const buttonVariants = cva(
       variant: {
         default:
           "bg-black text-white shadow hover:bg-black/80 focus:bg-black/80",
-        // destructive:
-        //   "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
           "border border-strokes text-black bg-transparent shadow-sm hover:underline focus:underline",
-        // secondary:
-        //   "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        // ghost: "hover:bg-accent hover:text-accent-foreground",
-        // link: "text-black underline-offset-4 hover:underline",
       },
       size: {
         default: "h-[50px] py-3 px-[33px]",
         sm: "h-[40px] py-[7px] px-[33px]",
         "extra-sm": "h-[40px] py-[7px] px-[22px]",
-        // lg: "h-10 rounded-md px-8",
-        // icon: "h-9 w-9",
       },
     },
     defaultVariants: {
@@ -39,17 +32,32 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  isLoading?: boolean
 }
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      isLoading = false,
+      children,
+      ...props
+    },
+    ref,
+  ) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        disabled={isLoading || props.disabled}
         {...props}
-      />
+      >
+        {isLoading ? <Spinner className="size-4" /> : children}
+      </Comp>
     )
   },
 )
