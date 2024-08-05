8f0cd56930997eb25377b6c2489250f2af9a58a7
WrL1n <80534285+WrL1n@users.noreply.github.com>
2024-08-05 10:58:38 +0400

    chore: add user list virtualization

--- app/[locale]/components/user-list.tsx
+++ app/[locale]/components/user-list.tsx
@@ -1,24 +1,46 @@
 "use client"
 
 import { useEntitiesContext } from "@/components/contexts/entities-context"
-import { ScrollToTop } from "@/components/ui"
+import { ScrollToTop, Spinner } from "@/components/ui"
 import { useScopedI18n } from "@/shared/i18n/client"
+import { type Ref, forwardRef } from "react"
+import { Virtuoso } from "react-virtuoso"
 import { UserListItem } from "./user-list-item"
 import { UserListWrapper } from "./user-list-wrapper"
 
 export const UserList = () => {
   const t = useScopedI18n("user-list")
-  const { users } = useEntitiesContext()
+  const { users, usersLoading, usersError } = useEntitiesContext()
+
+  if (usersError) {
+    return <div>{t("loading-error")}</div>
+  }
 
   return (
     <UserListWrapper>
-      <h1 className="text-title w-full">{t("title")}</h1>
-      <ul className="last:border-b last:border-border-strokes-secondary w-full md:max-w-[800px]">
-        {users?.map((user) => (
-          <UserListItem key={user.slug} user={user} />
-        ))}
-      </ul>
-
+      <h1 className="text-title w-full mb-4">{t("title")}</h1>
+      {usersLoading ? (
+        <div className="m-auto">
+          <Spinner />
+        </div>
+      ) : (
+        <div className="w-full md:max-w-[800px]">
+          <Virtuoso
+            useWindowScroll
+            totalCount={users.length}
+            itemContent={(index) => <UserListItem user={users[index]} />}
+            components={{
+              List: forwardRef((props, ref) => (
+                <ul
+                  {...props}
+                  ref={ref as Ref<HTMLUListElement>}
+                  className="last:border-b last:border-border-strokes-secondary w-full md:max-w-[800px]"
+                />
+              )),
+            }}
+          />
+        </div>
+      )}
       <ScrollToTop />
     </UserListWrapper>
   )
