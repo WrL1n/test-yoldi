92731ba6db8e0f268ad294bc6f5365e06da8f642
WrL1n <80534285+WrL1n@users.noreply.github.com>
2024-08-05 13:11:39 +0400

    fix: build

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import "@radix-ui/themes/styles.css"
 import "../../public/globals.css"
 import { Page } from "@/components/page"
 import { cn } from "@/shared/clsx"
